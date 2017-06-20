b044351ef0ff12c32c5485de879d233a4150093e
terzianp <pl.terzian@gmail.com>
2017-06-20 11:46:18 +0200

    Correcting mistakes

--- R/dmTable.R
+++ R/dmTable.R
@@ -9,31 +9,31 @@
 #'@param ControlRef a vector of ids corresponding to control samples.
 #'@param NARM will delete empty rows in data and platform if TRUE.
 #'
+#'@examples
+#'###generate random samples ids
+#'sampleId <- paste0(rep("sample", 10), 1:10)
+#'idProbes <- paste0(rep("probe", 10), 1:10)
+#'
+#'###generate data
+#'data <- matrix(runif(100,0,1), 10, 10)
+#'colnames(data) <- sampleId
+#'rownames(data) <- idProbes
+#'
+#'###generate exp_group
+#'exp_group <- as.data.frame(matrix(rep("x", 40),  10, 4), row.names = sampleId)
+#'exp_group$ref <- rep(c("01","11"),5)
+#
+#'###generate platfrom
+#'platform  <-as.data.frame(matrix(rep("x", 50),  10, 5), row.names = idProbes)
+#'
+#'##get indx of ref
+#'TumoralRef <- rownames(exp_group[exp_group$ref == "01", ])
+#'ControlRef <- rownames(exp_group[exp_group$ref == "11", ])
+#'
+#'###get a methylation differential table
+#'diff_table <- dmTable(data, platform, exp_group, ControlRef = ControlRef, TumoralRef = TumoralRef)
 #'
 #'@export
-#'@examples
-#`###generate random samples ids
-#`sampleId <- paste0(rep("sample", 10), 1:10)@param TumoralRef a vector of ids corresponding to Tumoral samples
-#`idProbes <- paste0(rep("probe", 10), 1:10)
-#`
-#`#*generate data
-#`data <- matrix(runif(100,0,1), 10, 10)
-#`colnames(data) <- sampleId
-#`rownames(data) <- idProbes
-#`
-#`###generate exp_group
-#`exp_group <- as.data.frame(matrix(rep(x, 40),  10, 4), row.names = sampleId)
-#`exp_group$ref <- rep(c("01","11"),5)
-#`
-#`###generate platfrom
-#`platform  <-as.data.frame(matrix(rep(x, 50),  10, 5), row.names = idProbes)
-#`
-#`##get indx of ref
-#`TumoralRef <- rownames(exp_group[exp_group$ref == "01", ])
-#`ControlRef <- rownames(exp_group[exp_group$ref == "11", ])
-#`
-#`###get a methylation differential table
-#`diff_table <- dmTable(data, platform, exp_group, ControlRef = ControlRef, TumoralRef = TumoralRef)
 
 
 
