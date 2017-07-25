212596be74dbc194b72d87ddb312cdc978e1d7c9
pterzian <pl.terzian@gmail.com>
2017-07-25 18:31:12 +0200

    labels become geneLabels
     labels is too much generic, it may be the source of warnings

--- R/clustdmProfil.R
+++ R/clustdmProfil.R
@@ -4,12 +4,12 @@
 #'
 #'@param mat a distance Matrix
 #'@param fill_NA boolean specifying if NA should be imputed, need to be kept TRUE for now if there are NA in the matrix
-#'@param labels A vector of genes names, can be extracted from a list of dmProfile with getProfileId
+#'@param geneLabels A vector of genes names or id, can be extracted from a list of dmProfile with getProfileId
 #'
 #'@return A list of 2 object, the hclust result and a list containing as much vectors of geneNames as the number of cluster selected.
 #'
 #'@export
-clustdmProfile <- function(mat, fill_NA = TRUE, labels){
+clustdmProfile <- function(mat, fill_NA = TRUE, geneLabels){
   
   if(fill_NA){
     for(i in 1:ncol(mat)){
@@ -19,8 +19,8 @@ clustdmProfile <- function(mat, fill_NA = TRUE, labels){
   }
   
   if(labels){
-    colnames(mat) <- labels
-    rownames(mat) <- labels
+    colnames(mat) <- geneLabels
+    rownames(mat) <- geneLabels
   }
   
   hclust_result <- stats::hclust(stats::as.dist(mat), method = "complete")
